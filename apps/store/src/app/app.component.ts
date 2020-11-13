cb06a0ea9091b104ad0384e8b2f5dbbdf4291c7c
kapilkhub <31470043+kapilkhub@users.noreply.github.com>
2020-11-14 01:27:29 +0400

    commit for lab 9

--- apps/store/src/app/app.component.ts
+++ apps/store/src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
-import {formatRating} from '@nx-shopping-system/store/util-formatters'
+import { formatRating } from '@nx-shopping-system/store/util-formatters';
+import { Game } from '@nx-shopping-system/util-interface';
 
 @Component({
   selector: 'nx-shopping-system-root',
@@ -8,13 +9,11 @@ import {formatRating} from '@nx-shopping-system/store/util-formatters'
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-
   /**
    *
    */
-  constructor(private http:HttpClient) {
-  }
+  constructor(private http: HttpClient) {}
   title = 'Board Game Hoard';
-  games = this.http.get<any[]>('/api/games');
+  games = this.http.get<Game[]>('/api/games');
   formatRating = formatRating;
 }
--- libs/store/feature-game-detail/src/lib/game-detail/game-detail.component.ts
+++ libs/store/feature-game-detail/src/lib/game-detail/game-detail.component.ts
@@ -1,27 +1,24 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, ParamMap } from '@angular/router';
-import {map, switchMap} from 'rxjs/operators'
+import { map, switchMap } from 'rxjs/operators';
 import { HttpClient } from '@angular/common/http';
-import {formatRating} from '@nx-shopping-system/store/util-formatters'
+import { formatRating } from '@nx-shopping-system/store/util-formatters';
+import { Game } from '@nx-shopping-system/util-interface';
 
 @Component({
   selector: 'nx-shopping-system-game-detail',
   templateUrl: './game-detail.component.html',
-  styleUrls: ['./game-detail.component.css']
+  styleUrls: ['./game-detail.component.css'],
 })
 export class GameDetailComponent implements OnInit {
-
- 
   constructor(private route: ActivatedRoute, private http: HttpClient) {}
 
   game$ = this.route.paramMap.pipe(
     map((params: ParamMap) => params.get('id')),
-    switchMap(id => this.http.get<any>(`/api/games/${id}`))
+    switchMap((id) => this.http.get<Game>(`/api/games/${id}`))
   );
 
   formatRating = formatRating;
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
